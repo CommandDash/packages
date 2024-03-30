0785777071cf71fa388dc89298485d8261ffbe5a
Keval Prajapati <keval.prajapatimus@gmail.com>
2024-03-30 21:19:00 +0530

    improves code parsing logic

--- commanddash/test/steps/prompt_query_test.dart
+++ commanddash/test/steps/prompt_query_test.dart
@@ -83,4 +83,64 @@ void main() {
             .isNotEmpty,
         true);
   });
+
+  test('Process a prompt request with code parser', () async {
+    handler.initProcessing();
+
+    messageStreamController.add(IncomingMessage.fromJson({
+      "method": "agent-execute",
+      "id": 1,
+      "params": {
+        "authdetails": {
+          "type": "gemini",
+          "key": EnvReader.get('GEMINI_KEY'),
+          "githubToken": ""
+        },
+        "inputs": [
+          {
+            "id": "736841542",
+            "type": "string_input",
+            "value": "Give dart function to calculate factorial of a number."
+          }
+        ],
+        "outputs": [
+          {"id": "90611917", "type": "default_output"}
+        ],
+        "steps": [
+          {
+            "type": "prompt_query",
+            "query": "<736841542>",
+            "post_process": {"type": "code"},
+            "output": "90611917"
+          },
+        ]
+      }
+    }));
+
+    final queue = StreamQueue<OutgoingMessage>(outwrapper.outputStream.stream);
+    var result = await queue.next;
+    expect(result, isA<StepMessage>());
+    expect(result.id, 1);
+    expect((result as StepMessage).kind, 'loader_update');
+    expect(result.args['kind'], 'message');
+    expect(result.args['message'], 'Preparing Result');
+    messageStreamController
+        .add(StepResponseMessage(1, 'loader_update', data: {}));
+    result = await queue.next;
+    expect(result, isA<ResultMessage>());
+    expect(result.id, 1);
+    expect((result as ResultMessage).message, 'TASK_COMPLETE');
+    expect((result as ResultMessage).message, isNotEmpty);
+    expect((result as ResultMessage).data.containsKey('90611917'), true);
+    expect((result as ResultMessage).data['90611917'], isA<DefaultOutput>());
+    expect(
+        ((result as ResultMessage).data['90611917'] as DefaultOutput).value !=
+            null,
+        true);
+    expect(
+        ((result as ResultMessage).data['90611917'] as DefaultOutput)
+            .value!
+            .isNotEmpty,
+        true);
+  });
 }
