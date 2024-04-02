dacd01133b07a31aee48dc851fe9682ae9765fd1
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2024-04-02 16:06:32 +0530

    (feat): Steps missing documentation

--- dash_agent/lib/steps/steps.dart
+++ dash_agent/lib/steps/steps.dart
@@ -1,10 +1,11 @@
 import 'package:dash_agent/data/datasource.dart';
 import 'package:dash_agent/variables/dash_input.dart';
 import 'package:dash_agent/variables/dash_output.dart';
+import 'package:meta/meta.dart';
 
-/// A single operation in a series of steps for a command.
+/// [Step] is a single operation in a series for a command.
 ///
-/// Supported Step Examples: [MatchDocumentStep], [WorkspaceQueryStep] etc
+/// Supported Step Examples: [MatchDocumentStep], [WorkspaceQueryStep], [PromptQueryStep], [AppendToChatStep]  etc
 abstract class Step {
   String get version;
 
@@ -13,7 +14,22 @@ abstract class Step {
   List<DashOutput?> get dashOutputs;
 }
 
-/// Find Matching Documents from provided datasources.
+/// Find matching documents from provided datasources.
+///
+/// Example:
+/// ```
+/// // Datasources
+/// final DataSource docsSource;
+///
+/// // Inputs
+/// final userQuery = StringInput('Your query', optional: false);
+/// final codeAttachment = CodeInput('Primary method');
+///
+/// // Output
+/// final matchingDocuments = MatchDocumentOuput()
+///
+/// return [MatchDocumentStep(query: '$userQuery$codeAttachment', dataSources: [docsSource], output: matchingDocuments)]
+/// ```
 class MatchDocumentStep extends Step {
   MatchDocumentStep(
       {required this.query, required this.dataSources, required this.output});
@@ -23,18 +39,17 @@ class MatchDocumentStep extends Step {
   /// Examples:
   /// ```
   /// final userInstruction = StringInput();
-  /// final codeAttachment = CodeInput();
-  /// query = "Apply the user's instruction: $userInstruction on the following code: $codeAttachment";
+  /// query = "$userInstruction";
   /// // or
-  /// final workspaceFiles = MultiCodeObject()
-  /// query = "Tests similar to $workspaceFiles";
+  /// final codeAttachment = CodeInput();
+  /// query = "Tests similar to $codeAttachment";
   /// ```
   final String query;
 
-  /// List of registered datasources to run the query match and return relevant documents.
+  /// List of datasources to query relevant documents from.
   final List<DataSource> dataSources;
 
-  /// All matching documents are stored as [output] and can be used in the further steps.
+  /// Stores matching documents to be used in the further steps.
   final MatchDocumentOuput output;
 
   @override
@@ -56,6 +71,25 @@ class MatchDocumentStep extends Step {
   List<DashOutput?> get dashOutputs => [output];
 }
 
+/// Find matching code snippets from user's workspace.
+///
+/// For (X) query or code input, semantically matching snippets (x1), (x2), (x3) etc are extracted.
+///
+/// Examples of (x):
+/// - 'Bloc files of $codeAttachment' => will return all top files (state, event, controller) from user's workspace relevant to the code attachment.
+/// - 'Repository Tests` => will return the top tests for repository object's from user's workspace.
+///
+/// These come in handy as context to be passed in the prompt. Remember this is semantic embedding matching and is not 100% accurate.
+///
+/// Usage:
+/// ```
+/// final userQuery = StringInput('Your query');
+/// final matchingDocuments = MatchDocumentOuput();
+///
+/// return [
+///   WorkspaceQueryStep(query: '$userQuery', output: matchingCode)
+/// ];
+/// ```
 class WorkspaceQueryStep extends Step {
   WorkspaceQueryStep({required this.query, required this.output});
 
@@ -79,7 +113,8 @@ class WorkspaceQueryStep extends Step {
   List<DashOutput?> get dashOutputs => [output];
 }
 
-/// Perform a Message Request to the LLM with your customized prompt.
+/// Perform a message request to the model with your customized prompt.
+///
 /// Example:
 /// ```
 /// final promptOutput = PromptOutput();
@@ -111,7 +146,12 @@ class PromptQueryStep extends Step {
   /// Append to chat or use in other steps.
   final PromptOutput? promptOutput;
 
+  /// [codeOutput] is experimental and is not adviced for usage. 🚨
+  ///
   /// Extract the last inline code snippet from the [promptOutput].
+  ///
+  /// Returns prompt output string itself if no code snippets between ``` <code snippet> ``` are found. Happens when the model returns the code directly as string without code blocks.
+  @experimental
   final CodeOutput? codeOutput;
 
   @override
@@ -134,6 +174,20 @@ class PromptQueryStep extends Step {
   List<DashOutput?> get dashOutputs => [promptOutput, codeOutput];
 }
 
+/// Appends the value to the chat.
+///
+/// This is the recommended way of sending response back to the user.
+///
+/// [value] could be string interpolated mix of inputs and outputs.
+///
+/// Example:
+/// ```
+/// final promptOutput = PromptOutput();
+/// return [
+///   PromptQueryStep(prompt:'Hi, what is the best way to add a List View in Flutter?', promptOutput: promptOutput),
+///   AppendToChatStep(value: '$promptOutput')
+/// ]
+/// ```
 class AppendToChatStep extends Step {
   AppendToChatStep({required this.value});
   final String value;
@@ -155,6 +209,12 @@ class AppendToChatStep extends Step {
   List<DashOutput?> get dashOutputs => [];
 }
 
+/// [ReplaceCodeStep] is experimental and is not adviced for usage. 🚨
+///
+/// Replaces the user's [CodeInput] with a resulting [CodeOutput].
+///
+/// A diff view of the changes is displayed within the editor with an option to accept or decline.
+@experimental
 class ReplaceCodeStep extends Step {
   ReplaceCodeStep(
       {required this.previousCode,
@@ -182,9 +242,12 @@ class ReplaceCodeStep extends Step {
   List<DashOutput?> get dashOutputs => [userAcceptDecision];
 }
 
-/// Use to decide if the steps should continue ahead.
+/// [ContinueDecisionStep] is experimental and is not adviced for usage. 🚨
 ///
-/// No, if [boolean] value is false.
+/// Acts as a decision maker if to continue with the next steps.
+///
+/// If [boolean] is false, terminates the steps and marks the agent process as done.
+@experimental
 class ContinueDecisionStep extends Step {
   final BooleanOutput boolean;
   ContinueDecisionStep({required this.boolean});
