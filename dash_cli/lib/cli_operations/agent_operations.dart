773cc8a4cd95866912e2ad4215428e6f49afc766
Yogesh Choudhary <youzendachoudhary22@gmail.com>
2024-04-02 16:30:46 +0530

    dash_cli version update + minCLIVersion Code Deleted

--- dash_cli/lib/cli_operations/agent_operations.dart
+++ dash_cli/lib/cli_operations/agent_operations.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:dash_cli/executors/agent_publish_executor.dart';
 import 'package:dash_cli/parsers/pubspec_parser.dart';
 import 'package:dash_cli/repository/agent_repository.dart';
 import 'package:dash_cli/template/simple_agent_template.dart';
@@ -10,7 +9,6 @@ import 'package:dash_cli/utils/spawn_isolate.dart';
 import 'package:dash_cli/utils/terminal_commands/run_terminal_command.dart';
 
 class AgentOperation {
-  final _agentPublishExecutor = AgentPublishExecutor();
   final _agentRepository = AgentRepository();
 
   Future<void> createAgentProject(String projectName) async {
@@ -118,7 +116,6 @@ class AgentOperation {
 
       final agentJson = await IsolateFunction.getAgentJson(projectDirectory);
 
-      final minCLIVersion = _agentPublishExecutor.getMinCLIVersion(agentJson);
       final agentName = pubSpecData.packageName;
       final agentDescription = pubSpecData.packageDescription;
       final agentVersion = pubSpecData.packageVersion;
@@ -126,7 +123,6 @@ class AgentOperation {
       agentJson['agent_name'] = agentName;
       agentJson['agent_description'] = agentDescription;
       agentJson['agent_version'] = agentVersion;
-      agentJson['cli_version'] = minCLIVersion;
 
       wtLog.log('- Agent configuration fetched');
       wtLog.updateSpinnerMessage('Publishing agent...');
